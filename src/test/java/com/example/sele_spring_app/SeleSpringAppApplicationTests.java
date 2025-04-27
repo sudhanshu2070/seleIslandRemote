5901a2d8b7de511f9876a7008d4cca1a11c1d2ec
sudhanshu2070 <sssallen2016@gmail.com>
2025-04-27 22:34:52 +0530

    Selenium driver 0.1

---
+++ src/test/java/com/example/sele_spring_app/SeleSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sele_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
